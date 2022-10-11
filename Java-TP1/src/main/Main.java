c45197fe6cce63c3d5ca96486eaa02b53077b22c
AlberVicky09 <al.rodriguezg.2018@alumnos.urjc.es>
2022-10-11 17:54:00 +0200

    1.2.3 Account table creation

--- Java-TP1/src/main/Main.java
+++ Java-TP1/src/main/Main.java
@@ -8,11 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		//Declare array of clients
 		ArrayList<Client> clientList;
+		//Declare array of accounts
+		ArrayList<Account> accountList;		
 		
 		//Load the clients
 		clientList = generateClients(Math.round(Math.random()*100));
 		//Display clients
 		displayClients(clientList);
+		
+		//Load the accounts
+		accountList = generateAccounts(clientList);
+		//Display accounts
+		displayAccounts(accountList);
 	}
 	
 	private static ArrayList<Client> generateClients(long numClients){
@@ -27,9 +34,25 @@ public class Main {
 	
 	private static void displayClients(ArrayList<Client> clientList){
 		//Iterate through all clients
-		for (Client client : clientList) {
+		for (Client client : clientList)
 			System.out.println(client.toString());
-		}
 	}
 
+	private static ArrayList<Account> generateAccounts(ArrayList<Client> clientList){
+		//Create aux array
+		ArrayList<Account> auxArray = new ArrayList<>();
+		
+		//Create a new account for each client
+		for (int i = 0; i < clientList.size(); i++)
+			auxArray.add(new CurrentAccount("Account" + i, clientList.get(i)));
+		
+		//Return the array
+		return auxArray;
+	}
+
+	private static void displayAccounts(ArrayList<Account> accountList) {
+		//Iterate through accounts
+		for (Account account : accountList)
+			System.out.println(account.toString());
+	}
 }
