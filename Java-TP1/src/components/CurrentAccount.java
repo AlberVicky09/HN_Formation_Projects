5b5a8129ab383111ea9b4f1dfcd3569fce0a7d2c
AlberVicky09 <al.rodriguezg.2018@alumnos.urjc.es>
2022-10-11 11:40:41 +0200

    1.2.2 Current and savings account

---
+++ Java-TP1/src/components/CurrentAccount.java
@@ -0,0 +1,9 @@
+package components;
+
+public class CurrentAccount extends Account{
+
+	public CurrentAccount(String l, Client c) {
+		super(l, c);
+	}
+	
+}
---
+++ Java-TP1/src/components/SavingsAccount.java
@@ -0,0 +1,9 @@
+package components;
+
+public class SavingsAccount extends Account{
+
+	public SavingsAccount(String l, Client c) {
+		super(l, c);
+	}
+
+}
